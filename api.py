9f9d6c826ca7210a02ab5adf1d558242a142af35
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2021-06-03 18:04:58 +0200

    api: format with black
    Format with black autoformatter.
    
    Bug: T283238
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>
    Change-Id: I8fb92bcf28a5565c4e08aa7e8cce3293c35abd8a

--- api.py
+++ api.py
@@ -10,13 +10,13 @@ from tjf.containers import Containers
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Run, '/api/v1/run/')
-api.add_resource(Show, '/api/v1/show/<name>')
-api.add_resource(List, '/api/v1/list/')
-api.add_resource(Delete, '/api/v1/delete/<name>')
-api.add_resource(Flush, '/api/v1/flush/')
-api.add_resource(Containers, '/api/v1/containers/')
+api.add_resource(Run, "/api/v1/run/")
+api.add_resource(Show, "/api/v1/show/<name>")
+api.add_resource(List, "/api/v1/list/")
+api.add_resource(Delete, "/api/v1/delete/<name>")
+api.add_resource(Flush, "/api/v1/flush/")
+api.add_resource(Containers, "/api/v1/containers/")
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
