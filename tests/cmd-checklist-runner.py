2a896dca0f9e16ca696910327c9985b4cfee9fc3
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2022-02-21 12:20:49 +0100

    tests/: refresh runner script
    Refresh runner script With latest code changes.
    
    While at it, refresh test definitions to drop explicit \n.
    
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>
    Change-Id: Iab0783e2862fd31ab2308e73da100f656a13570c

--- tests/cmd-checklist-runner.py
+++ tests/cmd-checklist-runner.py
@@ -29,77 +29,215 @@
 #
 
 import os
+import socket
+import platform
 import sys
 import argparse
 import subprocess
 import yaml
 import logging
+from dataclasses import dataclass, field
+from enum import Enum, auto
+from typing import Optional, List
+from pathlib import Path
+
+
+class InvalidConfigError(Exception):
+    """Class to represent an invalid configuration error."""
+
+
+class TestResult(Enum):
+    """Class to represent a test result."""
+
+    NOTRUN = auto()
+    OK = auto()
+    FAILED = auto()
+
+    def __str__(self):
+        return self.name.lower()
+
+
+@dataclass(frozen=True)
+class Command:
+    """Class to represent a command to be executed."""
+
+    cmd: str
+    retcode: Optional[int]
+    stdout: Optional[str]
+    stderr: Optional[str]
+
+
+@dataclass()
+class Test:
+    """Class to represent a test to be executed."""
+
+    name: str
+    spec: List[Command]
+    result: Optional[TestResult]
+
+    def get_prometheus_labels(self) -> str:
+        return f'test_name="{self.name}", test_result="{self.result}"'
+
+
+@dataclass(frozen=True)
+class Envvar:
+    """Class to represent an envvar."""
+
+    key: str
+    value: str
+
+
+@dataclass()
+class SystemInformation:
+    """Class to represent system information."""
+
+    hostname: str = field(init=False)
+    release: str = field(init=False)
+    os: str = field(init=False)
+
+    def __post_init__(self):
+        self.hostname = socket.gethostname()
+        self.release = platform.release()
+        self.os = "[unknown OS]"
+        try:
+            with open("/etc/os-release", "r") as f:
+                for line in f.readlines():
+                    if line.startswith("PRETTY_NAME="):
+                        self.os = line.split("PRETTY_NAME=")[1].strip().strip('"')
+                        break
+        except Exception:
+            pass
+
+    def get_prometheus_labels(self) -> str:
+        return f'hostname="{self.hostname}", os="{self.os}", release="{self.release}"'
+
+    def __str__(self):
+        return f"{self.hostname} {self.os} {self.release}"
+
+
+@dataclass()
+class Runner:
+    """Class to represent user configuration and runtime information."""
+
+    config_file: str
+    envvars: Optional[List[Envvar]]
+    tests: List[Test]
+    exit_code_fail: Optional[bool] = False
+    prometheus_file: Optional[str] = None
+    system_information: SystemInformation = field(init=False)
+    _PROMETHEUS_METRIC: str = field(init=False, default="cmd_checklist_runner")
+
+    def __post_init__(self):
+        self.system_information = SystemInformation()
+        self.config_file = os.path.abspath(self.config_file)
+
+    def _get_prometheus_file_content(self) -> str:
+        content = ""
+        for test in self.tests:
+            content += f"{self._PROMETHEUS_METRIC}{{"
+            content += f'config_file="{self.config_file}", '
+            content += test.get_prometheus_labels()
+            content += ", "
+            content += self.system_information.get_prometheus_labels()
+            content += "} 1\n"
+
+        return content
+
+    def write_prometheus_file(self) -> None:
+        if not self.prometheus_file:
+            return
+
+        content = self._get_prometheus_file_content()
+        logging.debug(
+            f"generating prometheus file '{self.prometheus_file}' with content:\n{content}"
+        )
+
+        temp_file = f"{self.prometheus_file}~"
+        try:
+            with open(temp_file, "w") as f:
+                f.write(self._get_prometheus_file_content())
+
+            Path(temp_file).rename(self.prometheus_file)
+        except Exception as e:
+            logging.error(e)
 
 
 def read_yaml_file(file):
     try:
         with open(file, "r") as stream:
-            try:
-                return [doc for doc in yaml.safe_load_all(stream)]
-            except yaml.YAMLError as e:
-                logging.error(e)
-                exit(2)
-    except FileNotFoundError as e:
+            return [doc for doc in yaml.safe_load_all(stream)]
+    except Exception as e:
         logging.error(e)
         exit(2)
 
 
 def validate_dictionary(dictionary, keys):
     if not isinstance(dictionary, dict):
-        logging.error(f"not a dictionary:\n{dictionary}")
-        return False
+        raise InvalidConfigError(f"not a dictionary:\n{dictionary}")
     for key in keys:
         if dictionary.get(key) is None:
-            logging.error(f"missing key '{key}' in dictionary:\n{dictionary}")
-            return False
-    return True
+            raise InvalidConfigError(f"missing key '{key}' in dictionary:\n{dictionary}")
 
 
-def load_envs(envvars_dict):
-    for envvars in envvars_dict:
-        logging.debug(envvars.items())
-        for envvar in envvars.items():
-            key = envvar[0]
-            value = envvar[1]
-            logging.debug(f"will try to set envvar: {key}={value}")
-            os.environ[key] = os.getenv(key, value)
+def load_envs(runner: Runner):
+    for envvar in runner.envvars:
+        key = envvar.key
+        value = envvar.value
+        logging.debug(f"will try to set envvar: {key}={value}")
+        os.environ[key] = os.getenv(key, value)
 
 
-def stage_validate_config(args):
+def stage_validate_config(args) -> Runner:
+    envvars = []
+    tests = []
+
     docs = read_yaml_file(args.config_file)
     for doc in docs:
         logging.debug(f"validating doc {doc}")
 
         for definition in doc:
             if definition.get("envvars", None):
-                load_envs(definition["envvars"])
+                for envvar in definition["envvars"]:
+                    for key, value in envvar.items():
+                        new_envvar = Envvar(key=key, value=value)
+                        envvars.append(new_envvar)
 
             if definition.get("name", None):
-                if not validate_dictionary(definition, ["name", "tests"]):
-                    logging.error(f"couldn't validate file '{args.config_file}'")
-                    return False
-                for test in definition["tests"]:
-                    if not validate_dictionary(test, ["cmd"]):
-                        logging.error(f"couldn't validate file '{args.config_file}'")
-                        return False
+                validate_dictionary(definition, ["name", "tests"])
 
-                ctx.checklist_dict = doc
+                test_cmds = []
+                for test in definition["tests"]:
+                    validate_dictionary(test, ["cmd"])
+                    cmd = Command(
+                        cmd=test.get("cmd"),
+                        retcode=test.get("retcode", None),
+                        stdout=test.get("stdout", None),
+                        stderr=test.get("stderr", None),
+                    )
+
+                    test_cmds.append(cmd)
+
+                test = Test(name=definition.get("name"), spec=test_cmds, result=TestResult.NOTRUN)
+                tests.append(test)
 
     logging.debug(f"'{args.config_file}' seems valid")
-    return True
+    return Runner(
+        config_file=args.config_file,
+        envvars=envvars,
+        tests=tests,
+        exit_code_fail=args.exit_code_fail,
+        prometheus_file=args.prometheus_output_file,
+    )
 
 
-def cmd_run(cmd, expected_retcode, expected_stdout, expected_stderr):
+def cmd_run(command: Command) -> bool:
     success = True
-    expanded_cmd = os.path.expandvars(cmd)
-    logging.debug(f"running command: {expanded_cmd}")
-    r = subprocess.run(cmd, capture_output=True, shell=True)
 
+    expanded_cmd = os.path.expandvars(command.cmd)
+    logging.debug(f"running command: {expanded_cmd}")
+    r = subprocess.run(expanded_cmd, capture_output=True, shell=True)
+
+    expected_retcode = command.retcode
     if expected_retcode is not None:
         if r.returncode != expected_retcode:
             logging.warning(
@@ -110,8 +248,9 @@ def cmd_run(cmd, expected_retcode, expected_stdout, expected_stderr):
     else:
         logging.debug("no retcode defined for command, ignoring")
 
+    expected_stdout = command.stdout
     if expected_stdout is not None:
-        stdout = r.stdout.decode("utf-8")
+        stdout = r.stdout.decode("utf-8").strip()
         if stdout != expected_stdout:
             logging.warning(
                 f"cmd '{expanded_cmd}', expected stdout '{expected_stdout}', but got '{stdout}'"
@@ -120,6 +259,7 @@ def cmd_run(cmd, expected_retcode, expected_stdout, expected_stderr):
     else:
         logging.debug("no stdout defined for command, ignoring")
 
+    expected_stderr = command.stderr
     if expected_stderr is not None:
         stderr = r.stderr.decode("utf-8").strip()
         if stderr != expected_stderr:
@@ -133,36 +273,49 @@ def cmd_run(cmd, expected_retcode, expected_stdout, expected_stderr):
     return success
 
 
-def test_run(test_definition):
-    logging.info("running test: {}".format(test_definition["name"]))
+def test_run(test: Test):
+    logging.info(f"running: {test.name}")
 
-    for test in test_definition["tests"]:
-        if cmd_run(
-            test["cmd"],
-            test.get("retcode", None),
-            test.get("stdout", None),
-            test.get("stderr", None),
-        ):
+    for command in test.spec:
+        if cmd_run(command):
             continue
 
-        logging.warning("failed test: {}".format(test_definition["name"]))
-        ctx.counter_test_failed += 1
+        logging.warning(f"failed test: {test.name}")
+        test.result = TestResult.FAILED
         return
 
-    ctx.counter_test_ok += 1
+    test.result = TestResult.OK
 
 
-def stage_run_tests(args):
-    for test_definition in ctx.checklist_dict:
-        test_run(test_definition)
+def stage_run_tests(runner: Runner):
+    load_envs(runner)
+
+    for test in runner.tests:
+        test_run(test)
 
 
-def stage_report():
+def stage_report(runner: Runner):
+
+    tests_ok = 0
+    tests_failed = 0
+    for test in runner.tests:
+        if test.result == TestResult.OK:
+            tests_ok += 1
+        elif test.result == TestResult.FAILED:
+            tests_failed += 1
+
     logging.info("---")
-    total = ctx.counter_test_ok + ctx.counter_test_failed
-    logging.info("--- passed tests: {}".format(ctx.counter_test_ok))
-    logging.info("--- failed tests: {}".format(ctx.counter_test_failed))
-    logging.info("--- total tests: {}".format(total))
+    logging.info(f"--- passed tests: {tests_ok}")
+    logging.info(f"--- failed tests: {tests_failed}")
+    logging.info(f"--- total tests: {tests_ok + tests_failed}")
+
+    runner.write_prometheus_file()
+
+    exit_code = 0
+    if runner.exit_code_fail and tests_failed > 0:
+        exit_code = 1
+
+    sys.exit(exit_code)
 
 
 def parse_args():
@@ -174,25 +327,29 @@ def parse_args():
         help="File with configuration and testcase definitions. Defaults to '%(default)s'",
     )
     parser.add_argument("--debug", action="store_true", help="debug mode")
-
+    parser.add_argument(
+        "--exit-code-fail",
+        action="store_true",
+        help="report in the exit code if a check fails",
+    )
+    parser.add_argument(
+        "--prometheus-output-file",
+        required=False,
+        help="If provided, generate a prom file with results from the testsuite",
+    )
     return parser.parse_args()
 
 
-class Context:
-    def __init__(self):
-        self.checklist_dict = None
-        self.counter_test_failed = 0
-        self.counter_test_ok = 0
-
-
-# global data
-ctx = Context()
+def stage_report_node_info(runner: Runner):
+    logging.info(f"--- {runner.system_information}")
+    logging.info("---")
 
 
 def main():
     args = parse_args()
 
-    logging_format = "[%(filename)s] %(levelname)s: %(message)s"
+    logging_format = "[%(asctime)s] %(levelname)s: %(message)s"
+    date_format = "%Y-%m-%d %H:%M:%S"
     if args.debug:
         logging_level = logging.DEBUG
     else:
@@ -203,12 +360,19 @@ def main():
     logging.addLevelName(
         logging.ERROR, "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.ERROR)
     )
-    logging.basicConfig(format=logging_format, level=logging_level, stream=sys.stdout)
+    logging.basicConfig(
+        format=logging_format, level=logging_level, stream=sys.stdout, datefmt=date_format
+    )
 
-    if not stage_validate_config(args):
+    try:
+        runner = stage_validate_config(args)
+    except InvalidConfigError as e:
+        logging.error(f"couldn't validate file '{args.config_file}': {e}")
         sys.exit(1)
-    stage_run_tests(args)
-    stage_report()
+
+    stage_report_node_info(runner)
+    stage_run_tests(runner)
+    stage_report(runner)
 
 
 if __name__ == "__main__":
